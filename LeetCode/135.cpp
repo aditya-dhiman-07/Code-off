7fbe7a6eae65e06d55df813ba50656c4c901ed82
Aditya <addhiman245@gmail.com>
2026-03-27 21:49:23 +0530

    commit2.2.1

--- LeetCode/135.cpp
+++ LeetCode/135.cpp
@@ -5,52 +5,25 @@ int getSum(int n) {
         return 0;
     return n + getSum(n - 1);
 }
-int candy(vector<int>& ratings) {
-    int n = ratings.size();
-    int count;
-    int i = 0;
-    int mincandy = n;
-    int a=0,d=0;
-    int asum = 0;
-    int dsum = 0;
-    int aeqsum = 1;
-    int deqsum = 1;
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> left(n, 1);
+        vector<int> right(n, 1);
 
-    while(1){
-        if(i>=n) break;
-        while(ratings[i]<ratings[i+1]&i<n-1){
-            asum++;
-            i++;
-        }
-        i++;
-        while(ratings[i]=ratings[i+1]){
-            aeqsum++;
-            i++;
-        }
-        while(ratings[i]>ratings[i+1]&i<n-1){
-            dsum++;
-            i++;
-        }
-        i++;
-        while(ratings[i]=ratings[i+1]){
-            deqsum++;
-            i++;
-        }
-        if(asum<=dsum){
-            a = getSum(asum)+(dsum-asum+1);
-        }
-        a = getSum(asum)+ ((aeqsum-1)*(asum-1));
-        d = getSum(dsum)+ ((deqsum-1)*(dsum-1));
-        mincandy = mincandy+a+d;
-        a=0,d=0;
-        asum = 0;
-        dsum = 0;
-        aeqsum = 1;
-        deqsum = 1;
+        for (int i = 1; i < n; i++)
+            if (ratings[i] > ratings[i-1])
+                left[i] = left[i-1] + 1;
+
+        for (int i = n-2; i >= 0; i--)
+            if (ratings[i] > ratings[i+1])
+                right[i] = right[i+1] + 1;
+
+        int total = 0;
+        for (int i = 0; i < n; i++)
+            total += max(left[i], right[i]);
+
+        return total;
     }
-    return mincandy;
-    
-}
 int main() {
     vector<int> ratings ;
     return 0;
