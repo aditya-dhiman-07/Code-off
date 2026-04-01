8e856e6c5a00423c656cbbd177d33ee8d9d8b2db
Aditya <addhiman245@gmail.com>
2026-04-01 22:42:59 +0530

    commit 2.1.3

--- NeetCode/array&hashing/1929.cpp
+++ NeetCode/array&hashing/1929.cpp
@@ -1,15 +1,2 @@
 #include <bits/stdc++.h>
 using namespace std;
-    vector<int> getConcatenation(vector<int>& nums) {
-        int n = nums.size();
-        vector <int> arr(2*n);
-        for(int i = 0;i<2*n;i++){
-            if(i<n) arr[i] = nums[i];
-            else arr[i]= nums[i-n];
-        }
-        return arr;
-    }
-int main() {
-    
-    return 0;
-}
