b6eb47e7e5cfea8f77117eb2665fc32f7fe32c25
Aditya <addhiman245@gmail.com>
2026-04-20 23:58:55 +0530

    commit 2.1.9

---
+++ NeetCode/lcp.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
+        string str1;
+        for (int i = 0; i < strs[0].size(); i++) {
+            int j = 1;
+            while (j < strs.size() && i < strs[j].size() && strs[0][i] == strs[j][i]) j++;
+            if (j == strs.size()) str1.push_back(strs[0][i]);
+            else break;
+        }
+        return str1;
+    }
+int main() {
+    
+    return 0;
+}
